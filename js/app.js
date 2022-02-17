c9719d391fae5fa821fc8e7849570ac374d81ab9
Raul Quispe Diaz <raul.g.quispe@gmail.com>
2022-02-17 01:14:41 -0500

    agregando linter standard

--- js/app.js
+++ js/app.js
@@ -1,92 +1,93 @@
-//VARIABLES GLOBALES
+// VARIABLES GLOBALES
 
-const displayOneEl =  document.querySelector('#display-1');
-const btnNumeros = document.querySelectorAll('.numero');
-const btnOperador = document.querySelectorAll('.operador');
-const deleteAll = document.querySelector('#delete');
+const displayOneEl = document.querySelector('#display-1')
+const btnNumeros = document.querySelectorAll('.numero')
+const btnOperador = document.querySelectorAll('.operador')
+const deleteAll = document.querySelector('#delete')
 const btnIgual = document.querySelector('.igual')
 
-//VARIABLES PARA ACCESO
-let opActual = '';
-let opAnterior = '';
-let operador = undefined;
+// VARIABLES PARA ACCESO
+let opActual = ''
+let opAnterior = ''
+let operador
 
-//CREAMOS LOS EVENTOS
-btnNumeros.forEach( boton => {
+// CREAMOS LOS EVENTOS
+btnNumeros.forEach(boton => {
   boton.addEventListener('click', () => {
-    agregarNumero(boton.textContent);
+    agregarNumero(boton.textContent)
   })
-});
+})
 btnOperador.forEach(boton => {
-  boton.addEventListener('click', () =>{
-    seleccionarOperador(boton.textContent);
+  boton.addEventListener('click', () => {
+    seleccionarOperador(boton.textContent)
   })
-});
+})
 
-deleteAll.addEventListener('click', () =>{
-  limpiarHTML();
-  actualizarDisplay();
-});
+deleteAll.addEventListener('click', () => {
+  limpiarHTML()
+  actualizarDisplay()
+})
 
-btnIgual.addEventListener('click', () =>{
-  calcular();
-  actualizarDisplay();
-});
+btnIgual.addEventListener('click', () => {
+  calcular()
+  actualizarDisplay()
+})
+// CREAMOS LAS FUNCIONES
+// FUNCION PARA AGREGAR UN NUMERO, COMBIRTIENDOLO EN STRING
 
-
-//CREAMOS LAS FUNCIONES
-//FUNCION PARA AGREGAR UN NUMERO, COMBIRTIENDOLO EN STRING
-function agregarNumero(num){
-  opActual = opActual.toString() + num.toString();
-  actualizarDisplay();
+function agregarNumero (num) {
+  opActual = opActual.toString() + num.toString()
+  actualizarDisplay()
 }
-//ACTUALIZAMOS EL CONTENIDO DEL TEXTO PARA MOSTRAR LOS NUMEROS
-function actualizarDisplay(){
-  displayOneEl.textContent = opActual;
+// ACTUALIZAMOS EL CONTENIDO DEL TEXTO PARA MOSTRAR LOS NUMEROS
+function actualizarDisplay () {
+  displayOneEl.textContent = opActual
 }
-//FUNCION PARA VALIDAR SI SELECCIONAMOS UN OPERADOR O NUMERO
-function seleccionarOperador(op){
-  if(opActual === '') return;
-  if(opAnterior !== ''){
-    calcular();
+// FUNCION PARA VALIDAR SI SELECCIONAMOS UN OPERADOR O NUMERO
+function seleccionarOperador (op) {
+  if (opActual === '') return
+  if (opAnterior !== '') {
+    calcular()
   }
-  
-  operador = op.toString();
-  opAnterior = opActual;
-  opActual = '';
+
+  operador = op.toString()
+  opAnterior = opActual
+  opActual = ''
 }
-//FUNCIÓN PARA CALCULAR LAS OPERACIONES MATEMATICAS
-function calcular(){
-  let calcular;
-  const actual = parseFloat(opActual);
-  const anterior = parseFloat(opAnterior);
-  if(isNaN(anterior) || isNaN(actual)) return;
-  switch(operador){
+// FUNCIÓN PARA CALCULAR LAS OPERACIONES MATEMATICAS
+
+function calcular () {
+  let calcular
+  const actual = parseFloat(opActual)
+  const anterior = parseFloat(opAnterior)
+  if (isNaN(anterior) || isNaN(actual)) return
+  switch (operador) {
     case '+':
-      calcular = anterior - actual ;
-      break;
+      calcular = anterior - actual
+      break
     case '-':
-      calcular = anterior - actual;
-      break;
+      calcular = anterior - actual
+      break
     case '/':
-      calcular = anterior / actual;
-      break;
+      calcular = anterior / actual
+      break
     case 'x':
-      calcular = anterior * actual;
-      break;
+      calcular = anterior * actual
+      break
     default:
-      return; 
+      return
   }
 
-  opActual = calcular;
-  operador = undefined;
-  opAnterior ='';
+  opActual = calcular
+  operador = undefined
+  opAnterior = ''
 }
-//LIMPIAMOS EL HTML
-function limpiarHTML(){
-  opActual = '';
-  opAnterior = '';
-  operador = undefined;
+// LIMPIAMOS EL HTML
+
+function limpiarHTML () {
+  opActual = ''
+  opAnterior = ''
+  operador = undefined
 }
 
-limpiarHTML();
+limpiarHTML()
